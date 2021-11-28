5dac301d291572d3e9bdd58619b02ba5f5b74ba4
sfr-frmnsyh <safri.firmansyah@gmail.com>
2021-11-28 17:47:24 +0700

    update login redirect & edit navigation admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,8 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <h1 class="text-2xl">Welcome to dashboard</h1>
+            <h1 class="text-2xl">Welcome to dashboard, click <a class="text-red-500" href="{{ route('/') }}">here</a> for back into home.</h1>
+            <h1 class="text-2xl"></h1>
         </div>
     </div>
 </x-app-layout>
