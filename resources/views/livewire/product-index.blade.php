2af80f7e134fcbe370c1f67af85fa75e911dfae2
sfr-frmnsyh <safri.firmansyah@gmail.com>
2021-11-28 07:46:52 +0700

    update title

--- resources/views/livewire/product-index.blade.php
+++ resources/views/livewire/product-index.blade.php
@@ -3,7 +3,7 @@
         <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
 
             <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl " href="#">
-                Latest Product
+                {{ $title }}
             </a>
 
             <div class="flex items-center" id="store-nav-content">
